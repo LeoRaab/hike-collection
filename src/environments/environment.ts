e8c45e5cbd6c4ab121578b2e633270ea355a619a
LeoRaab <leopold.raab@gmail.com>
2021-12-29 14:41:31 +0100

    added icons & environment dir

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'hike-it-48ea2',
+    appId: '1:590060903796:web:64d1bfb3b899e02dcb6021',
+    storageBucket: 'hike-it-48ea2.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDaaaQAsl_MDd1vqfYYahK3-1TAiVjbLwE',
+    authDomain: 'hike-it-48ea2.firebaseapp.com',
+    messagingSenderId: '590060903796',
+    measurementId: 'G-CKBJ2M9RNP',
+  },
   production: false
 };
 
