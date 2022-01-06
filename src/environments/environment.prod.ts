0c5d0b5f7759668a873a39f3e9644d9e51700986
LeoRaab <leopold.raab@gmail.com>
2022-01-06 15:55:07 +0100

    add firebase-cli to project and firebase config to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyAFhMt51ndcNOF7uO2jZqjZPyY2wVBT9-w',
+    authDomain: 'hike-collection.firebaseapp.com',
+    projectId: 'hike-collection',
+    storageBucket: 'hike-collection.appspot.com',
+    messagingSenderId: '697150268872',
+    appId: '1:697150268872:web:beaa773d737889740d0cfd',
+    measurementId: 'G-8VSJ3EFQN3'
+  },
   production: true
 };
