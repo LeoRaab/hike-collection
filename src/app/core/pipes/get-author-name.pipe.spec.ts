097751b53ef7c02de5820d041bc583f1cf85644d
LeoRaab <leopold.raab@gmail.com>
2022-01-07 20:13:19 +0100

    remove getAuthorNamePipe - was not used

--- src/app/core/pipes/get-author-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetAuthorNamePipe } from './get-author-name.pipe';
-
-describe('GetAuthorNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetAuthorNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
