fe25ea546fd6c64fb336197e8fb0c881bc470035
LeoRaab <leopold.raab@gmail.com>
2022-01-12 22:15:58 +0100

    change template-driven-form to reactive-form to get better validation methods

--- src/app/core/components/hike-form/hike-form.component.ts
+++ src/app/core/components/hike-form/hike-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import Hike from '../../models/hike.model';
 import {LoggerService} from '../../services/logger.service';
 import {ModalController} from '@ionic/angular';
@@ -10,13 +10,14 @@ import {PictureService} from '../../services/picture.service';
 import {AuthorService} from '../../services/author.service';
 import {LoadingSpinnerService} from '../../services/loading-spinner.service';
 import {MessageService} from '../../services/message.service';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-hike-form',
   templateUrl: './hike-form.component.html',
   styleUrls: ['./hike-form.component.scss'],
 })
-export class HikeFormComponent {
+export class HikeFormComponent implements OnInit {
   @Output() saveHike = new EventEmitter<Hike>();
 
   @Input() hike: Hike = {
@@ -25,11 +26,6 @@ export class HikeFormComponent {
     title: '',
     shortDescription: '',
     longDescription: '',
-    stats: {
-      duration: 0,
-      lowestPoint: 0,
-      highestPoint: 0
-    },
     location: {
       coordinates: {
         latitude: 0,
@@ -41,12 +37,40 @@ export class HikeFormComponent {
         city: ''
       }
     },
+    stats: {
+      duration: 0,
+      lowestPoint: 0,
+      highestPoint: 0
+    },
     pictureCollection: []
   };
+
+  hikeForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    shortDescription: ['', Validators.required],
+    longDescription: ['', Validators.required],
+    location: this.formBuilder.group({
+      coordinates: this.formBuilder.group({
+        latitude: ['', Validators.required],
+        longitude: ['', Validators.required],
+      }),
+      address: this.formBuilder.group({
+        street: ['', Validators.required],
+        zip: [0, Validators.required],
+        city: ['', Validators.required],
+      })
+    }),
+    stats: this.formBuilder.group({
+      duration: [0, Validators.required],
+      lowestPoint: [0, Validators.required],
+      highestPoint: [0, Validators.required]
+    })
+  });
 
   cameraSource = CameraSource;
 
   constructor(
+    private formBuilder: FormBuilder,
     private hikeService: HikeService,
     private configService: ConfigService,
     private modalController: ModalController,
@@ -58,11 +82,37 @@ export class HikeFormComponent {
   ) {
   }
 
-  handleSaveClick() {
-    this.saveHike.emit(this.hike);
+  ngOnInit() {
+    this.hikeForm.setValue({
+      title: this.hike.title,
+      shortDescription: this.hike.shortDescription,
+      longDescription: this.hike.longDescription,
+      location: {
+        coordinates: {
+          latitude: this.hike.location.coordinates.latitude,
+          longitude: this.hike.location.coordinates.longitude,
+        },
+        address: {
+          street: this.hike.location.address.street,
+          zip: this.hike.location.address.zip,
+          city: this.hike.location.address.city,
+        }
+      },
+      stats: {
+        duration: this.hike.stats.duration,
+        lowestPoint: this.hike.stats.lowestPoint,
+        highestPoint: this.hike.stats.highestPoint
+      }
+    });
   }
 
-  async showPictureListModal() {
+  public save() {
+    this.updateHikeWithFormValues();
+    console.log(this.hike);
+    //this.saveHike.emit(this.hike);
+  }
+
+  public async showPictureListModal() {
     const modal = await this.modalController.create({
       component: PictureModalPage,
       componentProps: {
@@ -97,6 +147,28 @@ export class HikeFormComponent {
         this.loadingSpinnerService.hide();
         this.loggerService.error('Getting picture failed!');
       });
+  }
+
+  private updateHikeWithFormValues() {
+    this.hike.title = this.hikeForm.value.title;
+    this.hike.shortDescription = this.hikeForm.value.shortDescription;
+    this.hike.longDescription = this.hikeForm.value.longDescription;
+    this.hike.location = {
+      coordinates: {
+        latitude: this.hikeForm.value.location.coordinates.latitude,
+        longitude: this.hikeForm.value.location.coordinates.longitude
+      },
+      address: {
+        street: this.hikeForm.value.location.address.street,
+        zip: this.hikeForm.value.location.address.zip,
+        city: this.hikeForm.value.location.address.city
+      }
+    };
+    this.hike.stats = {
+      duration: this.hikeForm.value.stats.duration,
+      lowestPoint: this.hikeForm.value.stats.lowestPoint,
+      highestPoint: this.hikeForm.value.stats.highestPoint
+    };
   }
 
   private updatePictureCollection(storageUrl: string) {
