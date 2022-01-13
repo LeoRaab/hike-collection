37c80876888ec1a68c2d9c19abba1d620c05f247
LeoRaab <leopold.raab@gmail.com>
2022-01-13 20:46:56 +0100

    activated LoggedInVerified & LoggedInNotVerified guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,47 +1,53 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import {AuthGuard} from './auth/auth.guard';
+import {LoggedInVerifiedGuard} from './auth/logged-in-verified.guard';
+import {LoggedInNotVerifiedGuard} from './auth/logged-in-not-verified.guard';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'detail/:id',
     loadChildren: () => import('./pages/detail/detail.module').then( m => m.DetailPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'edit/:id',
     loadChildren: () => import('./pages/edit/edit.module').then( m => m.EditPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'new',
     loadChildren: () => import('./pages/new/new.module').then( m => m.NewPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'author',
     loadChildren: () => import('./pages/author/author.module').then( m => m.AuthorPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'author/:id',
     loadChildren: () => import('./pages/author/author.module').then( m => m.AuthorPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
   },
   {
     path: 'author/friend/add/:authorId',
     loadChildren: () => import('./pages/add-friend/add-friend.module').then( m => m.AddFriendPageModule),
-    canActivate: [AuthGuard]
+    canActivate: [LoggedInVerifiedGuard]
+  },
+  {
+    path: 'add-friend',
+    loadChildren: () => import('./pages/add-friend/add-friend.module').then( m => m.AddFriendPageModule),
+    canActivate: [LoggedInVerifiedGuard]
+  },
+  {
+    path: 'verify',
+    loadChildren: () => import('./pages/verify/verify.module').then( m => m.VerifyPageModule),
+    canActivate: [LoggedInNotVerifiedGuard]
   },
   {
     path: 'login',
@@ -52,13 +58,13 @@ const routes: Routes = [
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
   {
-    path: 'verify',
-    loadChildren: () => import('./pages/verify/verify.module').then( m => m.VerifyPageModule)
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path: 'add-friend',
-    loadChildren: () => import('./pages/add-friend/add-friend.module').then( m => m.AddFriendPageModule),
-    canActivate: [AuthGuard]
+    path: '**',
+    redirectTo: 'home'
   }
 ];
 
